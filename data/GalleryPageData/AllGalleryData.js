4d28e151a4080775ad2a171a6b7be262f3393fce
vijitbalsori <vijitbalsori@gmail.com>
2024-05-19 21:04:18 +0530

    added new data

--- data/GalleryPageData/AllGalleryData.js
+++ data/GalleryPageData/AllGalleryData.js
@@ -1,26 +1,38 @@
 export const AllGalleryData = [
     {
-        image: "images/Ev1.jpg",
+        image: "images/bd 23_1.jpg",
         title: "Distribution of masks and sanitizers at Shraddha Home for orphans, Hyderabad",
     },
     {
-        image: "images/Ev3.jpg",
+        image: "images/marg 23_3.jpg",
         title: "Children being engrossed in a Kinder class at Old PUMS school, Vellore",
     },
     {
-        image: "images/Ev4.jpg",
+        image: "images/bd 23_2.jpg",
         title: "Distribution of food amongst locals in Kansalpet, Vellore",
     },
     {
-        image: "images/Ev6.jpg",
+        image: "images/bd 23_3.jpg",
         title: "A fun session with our elders at Vallalar Mudhiyor Illam, Vellore",
     },
     {
-        image: "images/Ev7.jpg",
+        image: "images/cdd 23_1.jpg",
         title: "Distribution of masks and sanitizers at Subash Children Home, Kanpur",
     },
     {
-        image: "images/Ev8.jpg",
+        image: "images/cdd 23_2.jpg",
+        title: "Notebook distribution at Karunalaya Children Home, Vellore",
+    },
+    {
+        image: "images/cdd 23_2.jpg",
+        title: "Notebook distribution at Karunalaya Children Home, Vellore",
+    },
+    {
+        image: "images/diwali p 1.jpg",
+        title: "Distribution of masks and sanitizers at Subash Children Home, Kanpur",
+    },
+    {
+        image: "images/diwali p 2.jpg",
         title: "Notebook distribution at Karunalaya Children Home, Vellore",
     },
 ];
