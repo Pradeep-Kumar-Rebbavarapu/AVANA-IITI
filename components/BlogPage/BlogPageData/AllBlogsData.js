e628f8468658695aa8cc5216b31f418af5bf14c2
vijitbalsori <vijitbalsori@gmail.com>
2024-05-21 23:36:21 +0530

    merged pull

---
+++ components/BlogPage/BlogPageData/AllBlogsData.js
@@ -0,0 +1,39 @@
+// AllBlogsData.jsx
+export const AllBlogsData = [
+    {
+      image: "images/blog1.png",
+      title: "Member 1",
+      description: "Member",
+    },
+    {
+      image: "images/blog1.png",
+      title: "Member 2",
+      description: "Member",
+    },
+    {
+      image: "images/blog1.png",
+      title: "Member 3",
+      description: "Member",
+    },
+    {
+      image: "images/blog1.png",
+      title: "Member 4",
+      description: "Member",
+    },
+    {
+      image: "images/blog1.png",
+      title: "Member 5",
+      description: "Member",
+    },
+    {
+      image: "images/blog1.png",
+      title: "Member 6",
+      description: "Member",
+    },
+    {
+      image: "images/blog1.png",
+      title: "Member 7",
+      description: "Member",
+    },
+  ];
+  
