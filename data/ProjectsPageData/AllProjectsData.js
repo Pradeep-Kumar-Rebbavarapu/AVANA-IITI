0cd22b2123e382476cbf133a6e47342a9a6e9f2b
vijitbalsori <vijitbalsori@gmail.com>
2024-05-24 20:21:24 +0530

    data update

--- data/ProjectsPageData/AllProjectsData.js
+++ data/ProjectsPageData/AllProjectsData.js
@@ -3,7 +3,7 @@ const AllProjectsData = [
         id: 1,
         title: "Margdarshan",
         description:
-            "description",
+            "Margdarshan is Avana's initiative to support underprivileged students from government schools through weekend tutoring sessions. It aims to bridge educational gaps by providing academic support to empower students academically. Every weekend, members and volunteers from Avana gather to lend a helping hand to students from government schools, offering academic support across various subjects, including mathematics, science, language arts, and more. Through dedicated mentorship, we strive to instill a love for learning, cultivate critical thinking skills, and unlock doors of opportunity that may have otherwise remained closed.",
         images: [
             "images/marg 23_1.jpg",
             "images/marg 23_2.jpg",
